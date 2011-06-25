0563126c5c2bb436e6e806d75d3e0e308b435a0b
Ben Nicholas <ben@ben-nicholas.com>
2011-06-25 01:20:12 -0700

    Added Results page mapping for features

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -13,6 +13,9 @@ module NavigationHelpers
     when /the home\s?page/
       '/'
 
+		when /the results page for (.*)/
+			"/results/#{$1}"
+
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
