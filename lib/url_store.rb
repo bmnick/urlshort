448938f95a995b764ef782fb7a55807d4f08a087
Ben Nicholas <ben@ben-nicholas.com>
2011-06-26 13:24:10 -0700

    added handling for slugless adding to the store

--- lib/url_store.rb
+++ lib/url_store.rb
@@ -1,10 +1,16 @@
+require 'digest/crc16'
+
 class UrlStore
 	def initialize
 		@mappings = Hash.new
 	end
 
 	def add_mapping slug, address
+		if slug.length == 0
+			slug = Digest::CRC16.hexdigest(address)
+		end
 		@mappings[slug] = address
+		slug
 	end
 
 	def get_mapping slug
