6cb1c053f5d803f15b20bea32ba4de52a932458e
Ben Nicholas <ben@ben-nicholas.com>
2011-06-25 22:35:48 -0700

    Results page rendering!

--- lib/url_short.rb
+++ lib/url_short.rb
@@ -1,7 +1,18 @@
 require 'sinatra/base'
 
 class UrlShort < Sinatra::Base
+	@@baseurl = 'http://urlshort.dev'
+
   get '/' do
     haml :index
   end
+
+	get '/results/:slug' do |slug|
+		@link = @@baseurl + '/' + slug
+		haml :results
+	end
+
+	post '/' do
+		redirect "/results/#{params[:slug]}"
+	end
 end
